7034e989bd4ee1ce1431a82d4a6c4208e6370724
CodeLewisYates <lewis_yates@ymail.com>
2021-03-01 21:55:59 +0000

    implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,11 @@ app.set('views', path.join(__dirname, 'views'));
 // Serving static files
 // path.join(__dirname, 'public'
 app.use(express.static(path.join(__dirname, 'public')));
+
+// implement cors
+app.use(cors());
+// allow non-simple requests from cross-origin requests for all routes
+app.options('*', cors());
 
 // set Security HTTP Headers
 app.use(
